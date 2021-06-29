60eb97399233567697ba3bb4c08e5d8603b0c459
tomas.dallas <tomas.dallas@almundo.com>
2021-06-29 20:51:48 -0300

    feat: fix zip extractio

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,12 @@ const PORT = 8000;
 app.get('/', (req: any, res: any) => res.send('Express + TypeScript Server'));
 
 // Schedule tasks to be run on the server.
-// cron.schedule('*/10 * * * *', function () {
-//   console.log('running a task every minute');
-//   console.log(getVaccineApplications());
-// });
+cron.schedule('*/10 * * * *', function () {
+  console.log('running a task every minute');
+  console.log(getVaccineApplications());
+});
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
-  getVaccineApplications();
+  // getVaccineApplications();
 });
--- src/jobs/downloadVaccineApplications.ts
+++ src/jobs/downloadVaccineApplications.ts
@@ -10,21 +10,17 @@ const DEFAULT_FILE_NAME_VACCINE_APPLICATIONS: string | undefined =
   process.env.DEFAULT_FILE_NAME_VACCINE_APPLICATIONS;
 const DEFAULT_FILE_NAME_VACCINE_APPLICATIONS_ZIP: string | undefined =
   process.env.DEFAULT_FILE_NAME_VACCINE_APPLICATIONS_ZIP;
-var AdmZip = require('adm-zip');
+
+const unzipper = require('unzipper');
 
 const extractZip = async (source: string | undefined): Promise<string> => {
   try {
-    console.log('About to extract zip', source);
-    // reading archives
-    var zip = new AdmZip(source);
-    zip.extractAllTo(
-      /*target path*/ DEFAULT_FILE_NAME_VACCINE_APPLICATIONS,
-      /*overwrite*/ true
-    );
-
-    console.log('Termino de extraer');
+    fs.createReadStream(source)
+      .pipe(unzipper.Extract({ path: ZIP_EXTRACT_FOLDER }))
+      .on('end', () => console.log('Finish zip extracion'));
     return DEFAULT_FILE_NAME_VACCINE_APPLICATIONS || '';
   } catch (error) {
+    console.log('Error while extracting', error);
     return 'ERRORASO PA';
   }
 };
@@ -32,22 +28,17 @@ const extractZip = async (source: string | undefined): Promise<string> => {
 export const getVaccineApplications = async (): Promise<
   Array<VaccineApplication>
 > => {
-  console.log('About to make request to get vaccines');
-  console.log(VACCINE_APPLICATIONS_URL);
-
   return axios({
     url: VACCINE_APPLICATIONS_URL,
     method: 'GET',
     responseType: 'arraybuffer',
   })
     .then((response: any) => {
-      console.log('saving', response.data.length);
       fs.writeFile(
         DEFAULT_FILE_NAME_VACCINE_APPLICATIONS_ZIP,
         response.data,
         (err: any) => {
           if (!err) {
-            console.log('finish saving');
             extractZip(DEFAULT_FILE_NAME_VACCINE_APPLICATIONS_ZIP);
           } else {
             console.log('ERROR GUARDADNO ARCHIVO');
