825dee0a0094d1f2901a9b3d005e75e902c773b9
tomas.dallas <tomas.dallas@almundo.com>
2021-07-05 18:01:09 -0300

    feat: added vaccine application service with base 4 services to implement

--- src/service/vaccineApplicationService.ts
+++ src/service/vaccineApplicationService.ts
@@ -1,1 +1,32 @@
-// export const;
+interface DoseDistributionByAgeGroup {}
+
+interface VaccineTypeDistributionByProvince {}
+
+interface ApplicationConditionsByAgeGroup {}
+
+const getDoseDistributionByAgeGroup = (): Array<DoseDistributionByAgeGroup> => {
+  return [];
+};
+
+const getVaccineTypeDistributionByProvince =
+  (): Array<VaccineTypeDistributionByProvince> => {
+    return [];
+  };
+
+// if none received, it means Argentina
+const getTotalVaccinesApplicated = (province: String | undefined): Number => {
+  return 0;
+};
+
+const getApplicationConditionsByAgeGroup = (
+  province: String | undefined
+): Array<ApplicationConditionsByAgeGroup> => {
+  return [];
+};
+
+export {
+  getDoseDistributionByAgeGroup,
+  getVaccineTypeDistributionByProvince,
+  getTotalVaccinesApplicated,
+  getApplicationConditionsByAgeGroup,
+};
